9b6ec01dbc84f0e67f3f0283f5848cecd1ebccf8
javsanzmon <100581169@alumnos.uc3m.es>
2026-09-25 11:08:38 +0200

    Modulo de input completo

--- input/input_usr.c
+++ input/input_usr.c
@@ -1,6 +1,8 @@
 #include  "input_usr.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
+#include <errno.h>
 
 int get_input(void) {
     // Variable initialization
@@ -14,22 +16,33 @@ int get_input(void) {
         char_read = getline(&line, &len, stdin);
         if (char_read == -1){
             free(line);
-            //Error handling here
-        }else{
-            parsed_input = strtol(line, &endptr, 10);
-            if (endptr == line) {
-                continue // strtol couldn't find any parseable digit, asks again for input
-            }else if (endptr != line){
-                /*
-                strtol outputs 0 if it doesn't parse any digit, meaning it
-                could be confused with the user input 0 (exit program).
-                So we check directly if strtol has done anything by comparing witha endptr.
-                */
-                return parsed_input;               
-            }else{
-                return 0; // Exit returns 0
-            }
+            exit(EXIT_FAILURE);
         }
+        errno = 0;
+        parsed_input = strtol(line, &endptr, 10);
+        if (endptr == line) {
+            printf("No parseable digits were found, try again...\n");
+            continue; // strtol couldn't find any parseable digit, asks again for input
+        }else if (errno == ERANGE && (parsed_input == LONG_MAX || parsed_input == LONG_MIN) || (errno != 0 && parsed_input == 0)){
+            // overflow state, errno = ERANGE and we ask for input again
+            printf("Input overflowed (string is either too big/small), try again...");
+            continue;
+        }else{
+            /*
+            strtol outputs 0 if it doesn't parse any digit, meaning it
+            could be confused with the user input 0 (exit program).
+            So we check directly if strtol has done anything by comparing with endptr.
+            */
+            if ((parsed_input >= 0 && parsed_input <= 7) && (*endptr == '\n' || *endptr == '\0')){ // Uses '' as "" references a string
+                free(line);
+                return (int)parsed_input;   
+            }else{
+                printf("Inputed value is not allowed (!= 0-7) or contains garbage (extra characters), try again...");
+                continue;
+            }
+           
+        } 
+        
     }
 
 }
